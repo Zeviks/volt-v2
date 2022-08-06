f5c1814a12e16cb739cc86ddb32d0350903e03d2
Natanael Pacheco <natepacheco716@gmail.com>
2022-08-06 01:18:45 -0500

    Added Flowbite.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
-/* @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"],
-  plugins: [
-    require('flowbite/plugin'), 
-    require('@tailwindcss/aspect-ratio')
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "node_modules/flowbite/**/*.{js,jsx,ts,tsx}"],
+  plugins: [require('flowbite/plugin'), 
+  require('@tailwindcss/aspect-ratio')
+],
   theme: {
     fontFamily: {
       display: ["Work Sans", "sans-serif"],
